75e6e542672d6ab000c86e568b4bb4eefc80299a
Jimmy Chen <jimmychen9785@gmail.com>
2022-06-07 21:23:14 -0700

    updated mock data

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -5,9 +5,10 @@ import { mockData } from '../mock-data';
 import { extractLocations } from '../api';
 
 describe('<CitySearch /> component', () => {
-  let CitySearchWrapper;
+  let locations, CitySearchWrapper;
   beforeAll(() => {
-    CitySearchWrapper = shallow(<CitySearch />);
+    locations = extractLocations(mockData);
+    CitySearchWrapper = shallow(<CitySearch locations={locations} />);
   });
 
   test('render text input', () => {
@@ -44,5 +45,17 @@ describe('<CitySearch /> component', () => {
         suggestions[i]
       );
     }
+  });
+
+  test('suggestion list match the query when changed', () => {
+    CitySearchWrapper.setState({ query: '', suggestions: [] });
+    CitySearchWrapper.find('.city').simulate('change', {
+      target: { value: 'Berlin' },
+    });
+    const query = CitySearchWrapper.state('query');
+    const filteredLocations = locations.filter((location) => {
+      return location.toUpperCase().indexOf(query.toUpperCase()) > -1;
+    });
+    expect(CitySearchWrapper.state('suggestions')).toEqual(filteredLocations);
   });
 });
