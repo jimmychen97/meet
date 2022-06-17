9e299a72eb974e93a455818e6050f71e74c0fcc6
Jimmy Chen <jimmychen9785@gmail.com>
2022-06-16 21:48:10 -0700

    Update NumberOfEvents.test.js

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -9,14 +9,7 @@ describe('<NumberOfEvents /> component', () => {
   });
 
   test('render text input box', () => {
-    expect(NumberOfEventsWrapper.find('.number-of-events')).toHaveLength(1);
-  });
-
-  test('renders text input changes', () => {
-    const numberOfEvents = NumberOfEventsWrapper.prop('numberOfEvents');
-    expect(NumberOfEventsWrapper.find('.number-of-events').prop('value')).toBe(
-      numberOfEvents
-    );
+    expect(NumberOfEventsWrapper.find('.NumberOfEvents')).toHaveLength(1);
   });
 
   test('change number of events when input changes', () => {
